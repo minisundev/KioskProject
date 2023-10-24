0f2a32b45bbe85449b12404f97a57d7f48c68425
Minsun <minisundev@gmail.com>
2023-10-24 19:46:52 +0900

    Made offer field in Order class

--- src/Order.java
+++ src/Order.java
@@ -2,8 +2,18 @@ import java.util.ArrayList;
 
 public class Order {
     public ArrayList<Product> instanceMenus;
+    private String offer = "";
 
     public Order() {
     }
+
+    public String getOffer(){
+        return this.offer;
+    }
+    public void setOffer(String offer){
+        this.offer = offer;
+    }
+
+
 
 }
